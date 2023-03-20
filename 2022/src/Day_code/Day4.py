d44322426da53b3fd7e143c99e5c64f14e1f2896
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-03-20 22:30:14 +0100

    Day for completed (again :( )

--- 2022/src/Day_code/Day4.py
+++ 2022/src/Day_code/Day4.py
@@ -1,9 +1,25 @@
 import re
 
 def handle_input(f):
+    # Create set with pair = {[start, end], [start,end]} for each pair, where each list is the section span
+    pairs = []
     for line in f.readlines():
-        pairs = re.split('[, -]', line.strip())
-        print(pairs)
+        pair = re.split('[-,]', line.strip())
+        first = set(range(int(pair[0]), int(pair[1])+1))
+        sec = set(range(int(pair[2]), int(pair[3])+1))
+        pairs.append([first, sec])
+    return pairs
 
 def solve_input(input):
-    pass
+    overlap = 0
+    contains = 0
+    for pair in input:
+        first_set = pair[0]
+        sec_set = pair[1]
+        shortest = min(len(first_set), len(sec_set))
+        intersect = first_set.intersection(sec_set)
+        if len(intersect) == shortest:
+            contains += 1
+        if intersect:
+            overlap += 1
+    return contains, overlap
