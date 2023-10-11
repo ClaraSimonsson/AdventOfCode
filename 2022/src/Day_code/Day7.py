7f44536323362a2cf410a9d78fffbb6fccbeff29
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-10-11 11:47:24 +0200

    Part 2 of Day7 completed

--- 2022/src/Day_code/Day7.py
+++ 2022/src/Day_code/Day7.py
@@ -5,11 +5,23 @@ def handle_input(f):
 
 def solve_input(input):
     dir_sizes = sumSizes(input)
-    sum = 0
+    tot_size = dir_sizes.get('/')
+    unused_space = 70000000 - tot_size
+    sum = 0     # total size of all directories with a size of at most 100 000
+    smallest_dir_size = tot_size        # Size of smallest directory to delete to reach 30 000 000 of unused space
     for k in dir_sizes:
         current_size = dir_sizes.get(k)
-        sum += current_size if current_size <= 100000 else 0
-    return sum
+        sum += add_size_below_limit(current_size)
+        if (if_enough_unused_space(unused_space + current_size)) and (current_size < smallest_dir_size):
+            smallest_dir_size = current_size
+    return [sum, smallest_dir_size]
+
+def add_size_below_limit(current_size):
+    return current_size if current_size <= 100000 else 0
+
+def if_enough_unused_space(tot_space):
+    return (tot_space) >= 30000000
+    
 
 def sumSizes(input):
     stack = []
