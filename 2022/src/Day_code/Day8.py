efe8c719365ce2bbe856e8e4b25f57b7cae72135
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-10-11 15:06:21 +0200

    Day 8 part 2 completed

--- 2022/src/Day_code/Day8.py
+++ 2022/src/Day_code/Day8.py
@@ -8,39 +8,67 @@ def handle_input(f):
 def solve_input(matrix):
     matrix_len = len(matrix)
     visible_trees = (matrix_len*4)-4
+    highest_scenic_score = 0
     for row in range(1, matrix_len-1):
         for col in range(1, matrix_len-1):
-            if if_visible(matrix, row, col):
+            visible, scenic_score = if_visible(matrix, row, col)
+            if visible:
                 visible_trees += 1
+            highest_scenic_score = scenic_score if (scenic_score > highest_scenic_score) else highest_scenic_score
+    print(highest_scenic_score)
     return visible_trees
 
 def if_visible(matrix, row, col):
-    return check_left(matrix, row, col) or check_right(matrix, row, col) or check_buttom(matrix, row, col) or check_top(matrix, row, col)
+    visible = False
+    left_vis, left_score = check_left(matrix, row, col)
+    right_vis, right_score = check_right(matrix, row, col)
+    buttom_vis, buttom_score = check_buttom(matrix, row, col)
+    top_vis, top_score = check_top(matrix, row, col)
+    if (left_vis or right_vis or buttom_vis or top_vis):
+        visible = True
+    score = left_score * right_score * buttom_score * top_score
+    return [visible, score]
     
 def check_left(matrix, row, col):
     vis = True
-    for i in range(0, col):
+    score = 0
+    for i in reversed(range(0, col)):
         if matrix[row][col] <= matrix[row][i]:
             vis = False
-    return vis
+            score += 1
+            break
+        score += 1
+    return [vis, score]
     
 def check_right(matrix, row, col):
     vis = True
+    score = 0
     for i in range(col+1, len(matrix)):
         if matrix[row][col] <= matrix[row][i]:
             vis = False
-    return vis
+            score += 1
+            break
+        score += 1
+    return [vis, score]
     
 def check_top(matrix, row, col):
     vis = True
-    for i in range(0, row):
+    score = 0
+    for i in reversed(range(0, row)):
         if matrix[row][col] <= matrix[i][col]:
             vis = False
-    return vis
+            score += 1
+            break
+        score += 1
+    return [vis, score]
     
 def check_buttom(matrix, row, col):
     vis = True
+    score = 0
     for i in range(row+1, len(matrix)):
         if matrix[row][col] <= matrix[i][col]:
             vis = False
-    return vis
+            score += 1
+            break
+        score += 1
+    return [vis, score]
